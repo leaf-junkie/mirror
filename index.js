ae040e812667a2c0c524196918ecf41f129c37d3
leaf-junkie <ragsdale.jar@gmail.com>
2021-05-17 14:00:16 -0400

    Add function to decrease face image size, then expand it (now with fewer pixels)

--- index.js
+++ index.js
@@ -6,14 +6,16 @@ const video = document.querySelector('.webcam');
 const canvas = document.querySelector('.video');
 const context = canvas.getContext('2d');
 const faceCanvas = document.querySelector('.face');
-const faceContext = canvas.getContext('2d');
+const faceContext = faceCanvas.getContext('2d');
 const faceDetector = new window.FaceDetector();
-console.log(video, canvas, faceCanvas, faceDetector);
+const SIZE = 10;
+const SCALE = 1.35;
+// console.log(video, canvas, faceCanvas, faceDetector);
 
 async function populateVideo() {
   const stream = await navigator.mediaDevices.getUserMedia({
-    // video: { width: 1280, height: 720}
-    video: { width: 1920, height: 1080}
+    video: { width: 1280, height: 720}
+    // video: { width: 1920, height: 1080}
   })
   video.srcObject = stream;
   await video.play();
@@ -28,12 +30,49 @@ async function detectFace() {
   // pass ref to video, image, or canvas
   const faces = await faceDetector.detect(video);
   faces.forEach(drawFace);
+  faces.forEach(censor);
   requestAnimationFrame(detectFace);
 }
 
 function drawFace(face) {
   const { width, height, top, left } = face.boundingBox;
-  console.log({width, height, top, left});
+  context.clearRect(0, 0, canvas.width, canvas.height)
+  context.strokeStyle = '#000000';
+  context.lineWidth = 2;
+  context.strokeRect(left, top, width, height);
+}
+
+function censor({ boundingBox: face }) {
+  faceContext.imageSmoothingEnabled = false;
+  faceContext.clearRect(0, 0, faceCanvas.width, faceCanvas.height)
+  // decrease face size 
+  faceContext.drawImage(
+    video,    // Where source comes from
+    face.x,   // Where to start source pull
+    face.y,        
+    face.width,
+    face.height,  
+    face.x,   // Where to start drawing
+    face.y,       
+    SIZE,   
+    SIZE   
+  );
+
+  const width = face.width * SCALE;
+  const height = face.width * SCALE;
+
+  // scale small image back up
+  faceContext.drawImage(
+    faceCanvas,
+    face.x,
+    face.y,
+    SIZE,
+    SIZE,
+    face.x - (width - face.width) / 2, 
+    face.y - (height - face.height) / 2, 
+    width,
+    height
+  );
 }
 
 populateVideo().then(detectFace);
